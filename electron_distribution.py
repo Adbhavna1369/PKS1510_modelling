76118cc74a4a74e5111de852be883c16d24ccdd2
Bhavna Adwani <73185067+Adbhavna1369@users.noreply.github.com>
2021-09-25 17:33:17 +0200

    updated code

--- electron_distribution.py
+++ electron_distribution.py
@@ -1,55 +1,54 @@
 import astropy.units as u
+from astropy.coordinates import Distance
 from agnpy.emission_regions import Blob
 import matplotlib.pyplot as plt
-from utils import AgnpyEC
 from agnpy.utils.plot import load_mpl_rc
-import logging
-from pathlib import Path
-from ruamel.yaml import YAML
-
-#from utils import load_sed_data, AgnpyEC
 
 
 # matplotlib adjustments
-yaml = YAML()
-log = logging.getLogger(__name__)
 load_mpl_rc()
 
-def ed_all_states():
-"""plot all the electron distributions at once"""
-log.info("plotting all the electron distributions together")
-colors = ["k", "navy", "dodgerblue", "mediumaquamarine"]
-states = ["low", "2012", "2015a", "2015b"] #,"hess_2016","magic_2016"
-
-# define figure
-fig, ax = plt.subplots()
-for state, color in zip(states, colors):
-# results dir
-    main_dir = Path(__file__).parent
-# load dictionary
-    pars = yaml.load(Path(f"{main_dir}/results/{state}/parameters.yaml"))
 
 # set the spectrum normalisation (total energy in electrons in this case)
-    spectrum_norm = 1e48 * u.Unit("erg")
+spectrum_norm = 1e48 * u.Unit("erg")
 # define the spectral function parametrisation through a dictionary
-    spectrum_dict = {
-        "type": "PowerLaw",
-        "parameters": {"p": pars["p2", "gamma_min": pars["gamma_min"], "gamma_max": pars["gamma_max"]},
+spectrum_dict_2012 = {
+    "type": "PowerLaw",
+    "parameters": {"p": 2.598, "gamma_min": 1, "gamma_max": 6e4},
+}
+spectrum_dict_low = {
+    "type": "PowerLaw",
+    "parameters": {"p": 3.255, "gamma_min": 1, "gamma_max": 6e4},
+}
+spectrum_dict_2015a = {
+    "type": "PowerLaw",
+    "parameters": {"p": 3.007, "gamma_min": 1, "gamma_max": 6e4},
+}
+spectrum_dict_2015b = {
+    "type": "PowerLaw",
+    "parameters": {"p": 3.295, "gamma_min": 1, "gamma_max": 6e4},
+}
+spectrum_dict_2016 = {
+    "type": "PowerLaw",
+    "parameters": {"p": 3.5, "gamma_min": 1, "gamma_max": 6e4},
 }
 # set the remaining quantities defining the blob
-    R_b = pars["R_b"]
-    B = pars["B"]
-    z = pars["z"]
-    delta_D = 25
-    Gamma = 20
-    blob = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, spectrum_dict)
-
+R_b = 2.75e11 * u.cm
+B = 0.35 * u.G
+z = 0.361
+delta_D = 25
+Gamma = 20
+blob_low = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, spectrum_dict_low)
+blob_2012 = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, spectrum_dict_2012)
+blob_2015a = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, spectrum_dict_2015a)
+blob_2015b = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, spectrum_dict_2015b)
+blob_2016 = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, spectrum_dict_2016)
 # plot the electron distribution
-blob.plot_n_e(gamma_power=2)
-plt.legend()
+blob_low.plot_n_e(gamma_power=2)
+blob_2012.plot_n_e(gamma_power=2)
+blob_2015a.plot_n_e(gamma_power=2)
+blob_2015b.plot_n_e(gamma_power=2)
+blob_2016.plot_n_e(gamma_power=2)
+
+plt.legend(["p = 2.8", "p = 3.255", "p = 2.598", "p = 3.007", "p = 3.295"],loc="upper right", fontsize ="xx-small")
 plt.show()
-ax.legend(
-        loc="upper center", fontsize=10, ncol=2,
-    )
-    Path("results").mkdir(exist_ok=True)
-fig.savefig(f"results/electron_distribution_all.png")
