ca900285b16555db203d1617cd67ecf16d3fe4cb
Bhavna Adwani <73185067+Adbhavna1369@users.noreply.github.com>
2022-08-01 15:51:42 +0200

    addition of the results from agnpy and gammapy

--- electron_distribution.py
+++ electron_distribution.py
@@ -48,12 +48,7 @@ spectrum_dict_2015b_mcmc = {
     "type": "BrokenPowerLaw",
     "parameters": {"p2": 3.139, "gamma_min": 1, "gamma_max": 2.991e4, "gamma_b" : 1.238e3 , "p1": 2.042 },
 }
-"""
-spectrum_dict_2016 = {
-    "type": "Broken PowerLaw",
-    "parameters": {"p2": 3.5, "gamma_min": 1, "gamma_max": 6e4},
-}
-"""
+
 # set the remaining quantities defining the blob
 R_b = 2.38e16 * u.cm
 #B = 0.35 * u.G
@@ -70,17 +65,17 @@ blob_2012 = Blob(R_b, z, delta_D, Gamma, 0.294 * u.G, spectrum_norm, spectrum_di
 blob_2015a = Blob(R_b, z, delta_D, Gamma, 0.276* u.G, spectrum_norm, spectrum_dict_2015a)
 blob_2015b = Blob(R_b, z, delta_D, Gamma, 0.379* u.G, spectrum_norm, spectrum_dict_2015b)
 
-#blob_2016 = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, spectrum_dict_2016)
-# plot the electron distribution
+#Plotting the electron distribution using the mcmc results 
 blob_low_mcmc.plot_n_e(gamma_power=2)
 blob_2012_mcmc.plot_n_e(gamma_power=2)
 blob_2015a_mcmc.plot_n_e(gamma_power=2)
 blob_2015b_mcmc.plot_n_e(gamma_power=2)
-#blob_2016.plot_n_e(gamma_power=2)
+
 plt.title ('Electron energy distribution(mcmc analysis)')
 plt.legend(["low_state", "2012", "2015a", "2015b"],loc="upper right", fontsize ="xx-small")
 plt.show()
 
+#Plotting the electron distribution using the results using agnpy
 blob_low.plot_n_e(gamma_power=2)
 blob_2012.plot_n_e(gamma_power=2)
 blob_2015a.plot_n_e(gamma_power=2)
